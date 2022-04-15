3aa76303255820862994d8e35612961100c89b9c
bradenmess <bradenmessbarger@yahoo.com>
2022-04-15 19:22:25 -0400

    Tweaked UI

--- TripSaver/settings.py
+++ TripSaver/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'UberPriceCheck.apps.UberpricecheckConfig',
+    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -117,6 +118,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
--- UberPriceCheck/forms.py
+++ UberPriceCheck/forms.py
@@ -3,4 +3,4 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 
 class InputForm(forms.Form):
     destination = forms.CharField()
-    distance = forms.IntegerField(validators=[MaxValueValidator(5), MinValueValidator(1)])
+    distance_in_miles = forms.IntegerField(widget=forms.NumberInput(attrs={'type':'range', 'step': '1', 'min': '1', 'max': '5', 'id':'myDistance'}))
