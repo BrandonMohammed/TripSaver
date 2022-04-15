c58efa6f9251c76c57ba54b8f76d1239e32c21c0
bradenmess <bradenmessbarger@yahoo.com>
2022-04-15 15:12:49 -0400

    Added base.html and bootstrap

--- UberPriceCheck/views.py
+++ UberPriceCheck/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'UberPriceCheck/home.html')
+    return render(request, 'UberPriceCheck/homepage.html')
