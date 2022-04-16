b907e093959e72ec6fae25532f5679b03ecc95bd
hgm18b <hgm18b@my.fsu.edu>
2022-04-16 13:57:10 -0400

    Created model for data returning from API call

--- UberPriceCheck/models.py
+++ UberPriceCheck/models.py
@@ -1,3 +1,15 @@
 from django.db import models
+from uber_rides.session import session
+from uber_rides.client import UberRidesClient
 
 # Create your models here.
+
+# This is where all the API calls begin
+# session = Session(server_token<TOEKEN>)
+# client = UberRidesClient(session)
+
+class PriceEstimate(models.Model):
+	start_latitude = models.FloatField()
+	start_longitude = models.FloatField()
+	end_latitute = modeld.FloatField()
+	end_longitude = models.FloatField()
