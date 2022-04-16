04fe0f3c6d4add951e19de8b3e2b4c57357cdbe5
hgm18b <hgm18b@my.fsu.edu>
2022-04-16 14:38:37 -0400

    -Update InputAddress model -Function to convert Address to Coordinates

--- UberPriceCheck/models.py
+++ UberPriceCheck/models.py
@@ -1,25 +1,27 @@
 from django.db import models
-from uber_rides.session import session
-from uber_rides.client import UberRidesClient
-import geopandas
-import geopy
+# from uber_rides.session import session
+# from uber_rides.client import UberRidesClient
+from geopy.geocoders import Nominatim
+
 
 # Create your models here.
 
-# This is where all the API calls begin
-# session = Session(server_token<TOEKEN>)
-# client = UberRidesClient(session)
-
 class PriceEstimate(models.Model):
-	display_name = models.CharField()
-	distance = models.FloatFied()
-	high_estimate = models.FloatField()
-	low_estimate = models.FloatField()
-	estimate = modeld.CharField()
+    display_name = models.CharField()
+    distance = models.FloatField()
+    high_estimate = models.FloatField()
+    low_estimate = models.FloatField()
+    estimate = models.CharField()
+
 
 class InputAddress(models.Model):
-	address = models.CharField()
-	city = models.CharField()
-	state = models.CharField()
-	zip_code = models.IntegerField()
-	distance = models.IntegerField()
+    address = models.CharField()
+    city = models.CharField()
+    state = models.CharField()
+    zip_code = models.IntegerField()
+    distance = models.IntegerField()
+
+    def get_Coordinates(self):
+        locator = Nominatim(user_agent="Uber_Price_Comparison")
+        location = locator.geocode(self.address + " " + self.city + " " + self.state + " " + self.zip_code)
+        return list([location.latitude, location.longitude])
