85759e54c37021768962ac00d98ec12ea765f22b
Tristan Morris <tristanbmorris@gmail.com>
2022-04-22 17:50:11 -0400

    changed database to localhosted postgresql

--- TripSaver/settings.py
+++ TripSaver/settings.py
@@ -77,8 +77,13 @@ WSGI_APPLICATION = 'TripSaver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'password',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
@@ -119,6 +124,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = '/static/'
+import os
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
